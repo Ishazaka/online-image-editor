52ebec4817c8bb569153412702c442eb9f78ac29
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-02-28 17:06:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,46 @@ addfilter();
 flipimages();
 
 }
+
+
+// to upload image on image-container
+uploadbtn.onchange = () =>{
+  resetfilter();
+  document.querySelector('.image-container').style.display = "block";
+  let reader = new FileReader();
+  reader.readAsDataURL(uploadbtn.files[0]);
+reader.onload = () =>{
+ image.setAttribute("src", reader.result);
+}
+
+}
+
+
+let sliders = document.querySelectorAll('.filter input[type="range"]');
+sliders.forEach( slider =>{
+  slider.addEventListener("input", addfilter)
+
+});
+
+function addfilter(){
+  image.style.filter = `blur(${filterA.value}px) contrast(${filterB.value}%) hue-rotate(${filterC.value}deg) sepia(${filterD.value}%)`;
+
+}
+
+let radiobtns = document.querySelectorAll('.flip-option input[type ="radio"]');
+
+radiobtns.forEach(radiobtn =>{
+  radiobtn.addEventListener("click", flipimages)
+
+})
+
+function flipimages(){
+  if(flipxbtn.checked){
+    image.style.transform = "scaleX(-1)"
+  }else if(flipybtn.checked){
+    image.style.transform = "scaleY(-1)"
+}else{
+   image.style.transform = "scale(1,1)"
+}
+}
+
